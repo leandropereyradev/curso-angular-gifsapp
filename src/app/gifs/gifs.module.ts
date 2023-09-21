68cfcfc13c3916dbb2ef2eb621ecabdab84c97f1
Leandro Pereyra <leandro-pereyra@hotmail.es>
2023-09-21 13:51:50 +0200

    Gifs: HomePageComponent

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [HomePageComponent],
+  exports: [HomePageComponent],
+  imports: [CommonModule],
 })
-export class GifsModule { }
+export class GifsModule {}

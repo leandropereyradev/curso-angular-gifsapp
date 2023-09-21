9dcea7593ac36af70266c0d775d2c8a1c30a261e
Leandro Pereyra <leandro-pereyra@hotmail.es>
2023-09-21 16:27:05 +0200

    Gifs: gifsService: HttpClient & HttpParams

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,12 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GifsService {
-  private _tagsHistory: string[] = [];
+  public gifList: Gif[] = [];
 
-  constructor() {}
+  private _tagsHistory: string[] = [];
+  private apiKey: string = '7QVsP8L7Wat7gOtGDHyTmNMm6umNmFuk';
+  private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
+
+  constructor(private http: HttpClient) {}
 
   get tagsHistory() {
     return [...this._tagsHistory];
@@ -28,5 +34,14 @@ export class GifsService {
     if (tag.length === 0) return;
 
     this.organizeHistory(tag);
+
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', '10')
+      .set('q', tag);
+
+    this.http
+      .get<SearchResponse>(`${this.serviceUrl}/search`, { params })
+      .subscribe((resp) => (this.gifList = resp.data));
   }
 }

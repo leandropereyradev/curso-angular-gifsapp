75b4a5e5b48ae788918501c90d1768ae9acd440e
Leandro Pereyra <leandro-pereyra@hotmail.es>
2023-09-21 15:42:16 +0200

    Gifs: gifsService

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,5 +1,7 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 
+import { GifsService } from '../../services/gifs.service';
+
 @Component({
   selector: 'gifs-search-box',
   templateUrl: './search-box.component.html',
@@ -9,9 +11,13 @@ export class SearchBoxComponent {
   @ViewChild('txtTagInput')
   public tagInput!: ElementRef<HTMLInputElement>;
 
+  constructor(private gifsService: GifsService) {}
+
   searchTag() {
     const newTag = this.tagInput.nativeElement.value;
 
-    console.log({ newTag });
+    this.gifsService.searchTag(newTag);
+
+    this.tagInput.nativeElement.value = '';
   }
 }
